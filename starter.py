5cb2f053707e4fd7d77784649f9c05443a69abf0
WitcherGeralt.im@gmail.com <WitcherGeralt.im@gmail.com@00d6c59f-2d1b-22ae-0c5b-c5f0823589bd>
2010-11-16 19:49:21 +0000

    starter

---
+++ starter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+#  BlackSmith starter
+#  BlackSmith.py
+
+# (c) WitcherGeralt [WitcherGeralt@rocketmail.com]
+
+from os import system as start_bot
+
+while True:
+	try:
+		start_bot('python ./BlackSmith.py')
+	except:
+		pass
