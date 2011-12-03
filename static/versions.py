3152508c827e81681edd0e8f118777b8ec0be15c
mrdoctorwho <mrdoctorwho@00d6c59f-2d1b-22ae-0c5b-c5f0823589bd>
2011-12-03 11:57:16 +0000

    Fix.

--- static/versions.py
+++ static/versions.py
@@ -1,13 +1,13 @@
 # caps\bot ver\core mod\bot rev\caps ver
 
-Caps = 'http://simpleapps.ru/'
+Caps = 'http://simpleapps.ru/caps#blacksmith-m.1'
 BOT_VER = 1
 CORE_MODE = 42
 BOT_REV = 80
 
 if os.access('.svn/entries', os.R_OK):
 	try:
-		BOT_REV = read_file('.svn/entries').readlines()[3].strip()
+		BOT_REV = int(file('.svn/entries').readlines()[3].strip())
 	except: 
 		pass
 		
